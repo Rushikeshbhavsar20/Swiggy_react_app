8c4577e922627a0bed79003457a004eb91b678b5
Rushikeshbhavsar20 <56561849+Rushikeshbhavsar20@users.noreply.github.com>
2024-09-24 17:34:56 +0530

    created React swiggy app succsefully

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement("h1",{},"dfdfdff")
+
+root.render(heading)
