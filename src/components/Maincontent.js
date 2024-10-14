468f5b84a4338003116e1481a3e81c47a9634ace
Rushikeshbhavsar20 <56561849+Rushikeshbhavsar20@users.noreply.github.com>
2024-10-14 17:14:44 +0530

    used usedeffect and learned consitional rendering

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -1,14 +1,39 @@
 import { Rescard } from "./Rescard";
-import {useState} from "react"
-import ResObj from "../../utils/constants";
+import {useState,useEffect} from "react"
+
+
 const Maincontent = () =>{
     
-    const[resList,setresList] = useState(ResObj);
-    const ListoffilterRes =  ResObj.filter((res)=>res.info.avgRating > 4.3);
+    const[resList,setresList] = useState([]);
+    const ListoffilterRes =  resList.filter((res)=>res.info.avgRating > 4.3);
     const Filter = ()=>{
         setresList(ListoffilterRes);
     };
+     
+    useEffect(() => {
+     fetchdata();
+      
+      
+    }, [])
+    
+    const fetchdata = async()=>{
+        const clouddata = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await clouddata.json();
+        const data = json.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        
+        if (data) {
+            setresList(data);
+        } else {
+            console.error("Data not found", json);
+            // Handle the case when data is undefined
+        }
+        
 
+    }
+    //conditonal rendering
+    if(resList.length == 0){
+        return <p className="shimmer">Loading.................</p>
+    } 
     return(
        <div className="mainc">
            <div className="search">
