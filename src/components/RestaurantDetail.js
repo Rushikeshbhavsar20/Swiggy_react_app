a0311ea612bcd367edef06244e6b7d238940119a
Rushikeshbhavsar20 <56561849+Rushikeshbhavsar20@users.noreply.github.com>
2024-10-18 18:18:21 +0530

    created a custom hook ,used sepration of concern method to keep code cleaner created a constant .js file so to collect all api url at one place

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,35 +1,9 @@
-import { useEffect, useState } from "react";
+import fetchresdata from "../../utils/useRestaurant";
 
 
 const RestaurantDetail = () => {
   
-  const[resdetail,setresdetail] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-  
-  useEffect(()=>{
-        fetchdata();
-  },[])
-
-
-    const fetchdata = async() =>{
-       try{
-        
-       const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.9974533&lng=73.78980229999999&restaurantId=328616&catalog_qa=undefined&submitAction=ENTER")
-       const  json = await data.json();
-       setresdetail(json);    
-       console.log(json);
-       
-       }catch(err){
-        setError("Failed to fetch data")
-        console.log(err);
-       }finally{
-          setLoading(false);
-       }
-   
-      
-    }
-
+      const resdetail = fetchresdata();
 
 
      
